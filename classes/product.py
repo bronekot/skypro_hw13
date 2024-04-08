989c85c54f6826ce3a62be4f6990a4db4d8e47e2
BroneKot <13498924+bronekot@users.noreply.github.com>
2024-04-08 23:33:13 +0300

    add price getter and setter

--- classes/product.py
+++ classes/product.py
@@ -2,23 +2,25 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
-    def get_price(self):
-        return self.price
+    @property
+    def price(self):
+        return self.__price
 
-    def set_price(self, new_price):
+    @price.setter
+    def price(self, new_price):
         if new_price <= 0:
             print("Некорректная цена (должна быть больше 0)")
             return
-        if new_price < self.price:
+        if new_price < self.__price:
             answer = input("Новая цена меньше старой, если вы хотите изменить введите Y")
             if answer.lower() != "y":
                 print("Цена НЕ изменена")
                 return
-        self.price = new_price
+        self.__price = new_price
         print("Цена изменена")
 
     def __str__(self):
-        return f"Товар: {self.name} ({self.description}) - Цена: {self.price}, Количество: {self.quantity}"
+        return f"Товар: {self.name} ({self.description}) - Цена: {self.__price}, Количество: {self.quantity}"
